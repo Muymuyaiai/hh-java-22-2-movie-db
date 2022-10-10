07bc1b1a7a737d6f0726bafdff622d5553b61e5e
leonmaar <myu@gmx.net>
2022-10-10 16:20:58 +0200

    Add optional function

---
+++ backend/src/test/java/de/neuefische/backend/service/MovieServiceTest.java
@@ -0,0 +1,28 @@
+package de.neuefische.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+    @Test
+    void getAllMovies() {
+    }
+
+    @Test
+    void getMovieByID() {
+    }
+
+    @Test
+    void addMovie() {
+    }
+
+    @Test
+    void deleteMovieById() {
+    }
+
+    @Test
+    void updateMovie() {
+    }
+}
